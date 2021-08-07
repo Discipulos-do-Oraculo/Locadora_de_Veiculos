047db787874f71d2934b9723815488d0b6127221
Marcos Matos <marcoscaiobel@gmail.com>
2021-08-06 22:21:23 -0300

    Classe pessoaBase

--- LocadoraVeiculo.Dominio/PessoaBase.cs
+++ LocadoraVeiculo.Dominio/PessoaBase.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace LocadoraVeiculo.Dominio
 {
-    class PessoaBase
+    public class PessoaBase
     {
+        int id, numero;
+        string nome, logradouro, bairro, cidade, estado, telefone, celular;
+
+        public int Id { get => id; set => id = value; }
+        public int Numero { get => numero; set => numero = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Logradouro { get => logradouro; set => logradouro = value; }
+        public string Bairro { get => bairro; set => bairro = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+        public string Celular { get => celular; set => celular = value; }
     }
 }

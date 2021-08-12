7dbfcd589b59b34887ced7d1ec3a301ca889a859
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-08-12 17:33:13 -0300

    início integração tela cliente jurídico com controladores

---
+++ LocadoraVeiculos.WindowsForms/Features/Clientes/ClienteCNPJ/TabelaClienteCnpjControl.cs
@@ -0,0 +1,52 @@
+﻿using LocadoraVeiculo.Dominio.ClienteModule;
+using LocadoraVeiculos.WindowsForms.Shared;
+using System;
+using System.Collections.Generic;
+
+using System.Windows.Forms;
+
+namespace LocadoraVeiculos.WindowsForms.Features.Clientes.ClienteCNPJ
+{
+    public partial class TabelaClienteCnpjControl : UserControl
+    {
+        public TabelaClienteCnpjControl()
+        {
+            InitializeComponent();
+            gridClienteCnpj.ConfigurarGridZebrado();
+            gridClienteCnpj.ConfigurarGridSomenteLeitura();
+            gridClienteCnpj.Columns.AddRange(ObterColunas());
+        }
+
+        public DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Nome", HeaderText = "Nome"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Cnpj", HeaderText = "Cnpj"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Telefone", HeaderText = "Telefone"},
+            };
+
+            return colunas;
+        }
+
+        public int ObtemIdSelecionado()
+        {
+            return gridClienteCnpj.SelecionarId<int>();
+        }
+
+        public void AtualizarRegistros(List<ClienteCnpj> clientes)
+        {
+            gridClienteCnpj.Rows.Clear();
+
+            foreach (ClienteCnpj cliente in clientes)
+            {
+                gridClienteCnpj.Rows.Add(cliente.Id, cliente.Nome, cliente.Cnpj, cliente.Telefone);
+
+            }
+        }
+    }
+}
---
+++ LocadoraVeiculos.WindowsForms/Features/Clientes/ClienteCNPJ/TelaClienteCnpj.cs
@@ -0,0 +1,36 @@
+﻿using LocadoraVeiculo.Dominio.ClienteModule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraVeiculos.WindowsForms.Features.Clientes.ClienteCNPJ
+{
+    public partial class TelaClienteCnpj : Form
+    {
+        private ClienteCnpj cliente;
+        public ClienteCnpj ClienteCnpj
+        {
+            get { return cliente; }
+            set
+            {
+                cliente = value;
+
+                textBoxId.Text = cliente.Id.ToString();
+                txtNome.Text = cliente.Nome;
+                txtCelular.Text = cliente.Celular;
+                txtCidade.Text = cliente.Cidade;
+
+            }
+        }
+        public TelaClienteCnpj()
+        {
+            InitializeComponent();
+        }
+    }
+}

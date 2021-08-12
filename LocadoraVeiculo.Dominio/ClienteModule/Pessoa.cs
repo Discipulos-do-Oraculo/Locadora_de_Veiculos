30dc8391de953618acf38802a844a7019f55e106
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-08-12 16:11:35 -0300

    controlador e domínio cliente cnpj

--- LocadoraVeiculo.Dominio/ClienteModule/Pessoa.cs
+++ LocadoraVeiculo.Dominio/ClienteModule/Pessoa.cs
@@ -1,25 +1,18 @@
 ﻿using LocadoraVeiculo.Dominio.shared;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LocadoraVeiculo.Dominio
 {
     public class Pessoa : EntidadeBase
     {
-        private int numero;
-        private string nome, logradouro, bairro, cidade, estado, telefone, celular;
-
-        public int Numero { get => numero; set => numero = value; }
+        private string nome, cidade, estado, telefone, celular, email, endereco;       
         public string Nome { get => nome; set => nome = value; }
-        public string Logradouro { get => logradouro; set => logradouro = value; }
-        public string Bairro { get => bairro; set => bairro = value; }
         public string Cidade { get => cidade; set => cidade = value; }
         public string Estado { get => estado; set => estado = value; }
         public string Telefone { get => telefone; set => telefone = value; }
         public string Celular { get => celular; set => celular = value; }
+        public string Email { get => email; set => email = value; }
+        public string Endereco { get => endereco; set => endereco = value; }
 
         public override string Validar()
         {

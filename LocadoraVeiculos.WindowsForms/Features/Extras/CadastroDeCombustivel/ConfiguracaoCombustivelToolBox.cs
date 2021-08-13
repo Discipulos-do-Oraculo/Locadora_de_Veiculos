85eec58a6c2fa5fd5569559fc4df95cb93e59fb7
nunnes3 <lilcelonunes@gmail.com>
2021-08-13 17:03:17 -0300

    Criação da e Interação do Controlador com a Tela Combustivel

---
+++ LocadoraVeiculos.WindowsForms/Features/Extras/CadastroDeCombustivel/ConfiguracaoCombustivelToolBox.cs
@@ -0,0 +1,26 @@
+﻿using LocadoraVeiculos.WindowsForms.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.WindowsForms.Features.Extras.CadastroDeCombustivel
+{
+    public class ConfiguracaoCombustivelToolBox : IConfiguracaoToolBox
+    {
+        public string ToolTipAdicionar {get { return "Cadastro Combustível"; } }
+
+        public string TipoCadastro { get { return "Cadastro de Combustível"; } }
+
+        public string ToolTipEditar { get { return "Editar Combustível"; } }
+
+        public string ToolTipExcluir { get { return "Não disponível"; } }
+
+        public string ToolTipFiltro { get { return "Não disponível"; } }
+
+        public string ToolTipAgrupar { get { return "Não disponível"; } }
+
+        public string ToolTipDesAgrupar { get { return "Não disponível"; } }
+    }
+}
---
+++ LocadoraVeiculos.WindowsForms/Features/Extras/CadastroDeCombustivel/OperacoesCombustivel.cs
@@ -0,0 +1,88 @@
+﻿using LocadoraVeiculo.Dominio.CombustivelModule;
+using LocadoraVeiculos.Controlador.GasolinaModule;
+using LocadoraVeiculos.WindowsForms.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraVeiculos.WindowsForms.Features.Extras.CadastroDeCombustivel
+{
+    public class OperacoesCombustivel : ICadastravel
+    {
+        private readonly ControladorCombustivel controlador = null;
+        private TabelaCombustivelControl tabelaCombustivel = null;
+
+        public OperacoesCombustivel(ControladorCombustivel ctrl)
+        {
+            controlador = ctrl;
+            tabelaCombustivel = new TabelaCombustivelControl();
+        }
+
+        public void AgruparRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditarRegistro()
+        {
+            int id = tabelaCombustivel.ObtemIdSelecionado();
+
+            if (id == 0)
+            {
+                MessageBox.Show("Selecione um grupo para poder editar!", "Edição de Grupo de Veículos",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            Combustivel combustivelSelecionado = controlador.SelecionarPorId(id);
+
+            TelaCombustivel tela = new TelaCombustivel();
+
+            tela.Combustivel = combustivelSelecionado;
+
+            if (tela.ShowDialog() == DialogResult.OK)
+            {
+                controlador.Editar(id, tela.Combustivel);
+
+                List<Combustivel> combustiveis = controlador.SelecionarTodos();
+
+                tabelaCombustivel.AtualizarRegistros(combustiveis);
+
+                TelaInicial.Instancia.AtualizarRodape($"Combustível: [{tela.Combustivel.Nome}] editado com sucesso");
+            }
+        }
+
+        public void ExcluirRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InserirNovoRegistro()
+        {
+            TelaCombustivel tela = new TelaCombustivel();
+
+            if (tela.ShowDialog() == DialogResult.OK)
+            {
+                controlador.InserirNovo(tela.Combustivel);
+
+                List<Combustivel> combustiveis = controlador.SelecionarTodos();
+
+                tabelaCombustivel.AtualizarRegistros(combustiveis);
+
+                TelaInicial.Instancia.AtualizarRodape($"Combustível : [{tela.Combustivel.Nome}] inserido com sucesso");
+            }
+        }
+
+        public UserControl ObterTabela()
+        {
+            List<Combustivel> combustiveis = controlador.SelecionarTodos();
+
+            tabelaCombustivel.AtualizarRegistros(combustiveis);
+
+            return tabelaCombustivel;
+        }
+    }
+}

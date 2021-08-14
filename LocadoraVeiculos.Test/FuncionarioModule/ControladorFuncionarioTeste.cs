ade2f805953e4cfda7697fe8fcd9ebdc20c9630f
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-08-13 22:24:52 -0300

    Implementação testes Funcionário

---
+++ LocadoraVeiculos.Test/FuncionarioModule/ControladorFuncionarioTeste.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using LocadoraVeiculo.Dominio.ClienteModule;
+using LocadoraVeiculos.Controlador;
+using LocadoraVeiculos.Controlador.ClienteModule.CondutorControlador;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraVeiculos.Test.FuncionarioModule
+{
+    [TestClass]
+    [TestCategory("Controladores")]
+    public class ControladorFuncionarioTeste
+    {
+        private ControladorColaborador controlador = null;
+
+        public ControladorFuncionarioTeste()
+        {
+            controlador = new ControladorColaborador();
+            Db.Update("DELETE FROM [TBCOLABORADOR]");
+        }
+
+        [TestMethod]
+        public void DeveInserir_Funcionario()
+        {
+            //arrange
+            var novoFuncionario = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            controlador.InserirNovo(novoFuncionario);
+
+            //assert
+            var funcionarioEncontrado = controlador.SelecionarPorId(novoFuncionario.Id);
+            funcionarioEncontrado.Should().Be(novoFuncionario);
+        }
+
+        [TestMethod]
+        public void DeveAtualizar_Funcionario()
+        {
+            //arrange
+            var funcionario = new  Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            controlador.InserirNovo(funcionario);
+
+            var novoFuncionario = new Colaborador("lito", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+
+            //action
+            controlador.Editar(funcionario.Id, novoFuncionario);
+
+            //assert
+            Colaborador funcionarioAtualizado = controlador.SelecionarPorId(funcionario.Id);
+            funcionarioAtualizado.Should().Be(novoFuncionario);
+        }
+
+        [TestMethod]
+        public void DeveExcluir_Funcionario()
+        {
+            //arrange            
+            var funcionario = new Colaborador("lito", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            controlador.InserirNovo(funcionario);
+
+            //action            
+            controlador.Excluir(funcionario.Id);
+
+            //assert
+            Colaborador funcionarioEncontrado = controlador.SelecionarPorId(funcionario.Id);
+            funcionarioEncontrado.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void DeveSelecionar_Funcionario_PorId()
+        {
+            //arrange
+            var funcionario = new Colaborador("lito", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            controlador.InserirNovo(funcionario);
+
+            //action
+            Colaborador funcionarioEncontrado = controlador.SelecionarPorId(funcionario.Id);
+
+            //assert
+            funcionarioEncontrado.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void DeveSelecionar_TodosFuncionarios()
+        {
+            //arrange
+            var gv1 = new Colaborador("lito", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+            controlador.InserirNovo(gv1);
+
+            var gv2 = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+            controlador.InserirNovo(gv2);
+
+            var gv3 = new Colaborador("Charles Visit", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+            controlador.InserirNovo(gv3);
+
+            //action
+            var grupoDeVeiculoss = controlador.SelecionarTodos();
+
+            //assert
+            grupoDeVeiculoss.Should().HaveCount(3);
+            grupoDeVeiculoss[0].Nome.Should().Be("lito");
+            grupoDeVeiculoss[1].Nome.Should().Be("zezo vitória");
+            grupoDeVeiculoss[2].Nome.Should().Be("Charles Visit");
+        }
+    }
+}
---
+++ LocadoraVeiculos.Test/FuncionarioModule/FuncionarioTeste.cs
@@ -0,0 +1,183 @@
+﻿using FluentAssertions;
+using LocadoraVeiculo.Dominio.ClienteModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraVeiculos.Test.FuncionarioModule
+{
+    [TestClass]
+    [TestCategory("Dominio")]
+    public class FuncionarioTeste
+    {
+        [TestMethod]
+        public void DeveRetornar_FuncionarioValido()
+        {
+            //arrange
+            var funcionario = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com","lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime (2021, 08, 13), 4500);
+
+            //action
+            var resultado = funcionario.Validar();
+
+            //assert
+            resultado.Should().Be("ESTA_VALIDO");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Nome()
+        {
+            //arrange
+            var cliente = new Colaborador("", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo nome é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Cpf()
+        {
+            //arrange
+            var cliente = new  Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo CPF é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Rg()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo Rg é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_TelefoneNulo()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo telefone é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_TelefoneInvalido()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "123", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo Telefone está inválido");
+        }
+
+        [TestMethod]
+        public void DeveValidar_EmailNulo()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo email é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_EmailInvalido()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "zezo.@gata", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo Email está inválido");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Cidade()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo cidade é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Endereco()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo endereço é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_CelularNulo()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo celular é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_CelularInvalido()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "123", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo Celular está inválido");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Estado()
+        {
+            //arrange
+            var cliente = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+
+            //action
+            var resultado = cliente.Validar();
+
+            //assert
+            resultado.Should().Be("O campo estado é obrigatório");
+        }
+    }
+}

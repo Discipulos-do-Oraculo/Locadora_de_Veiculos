b5f7d409d114838ec230bab6c42cb85d7cb2604b
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-08-16 18:56:50 -0300

    placas corrigidas

--- LocadoraVeiculos.Test/FuncionarioModule/ControladorFuncionarioTeste.cs
+++ LocadoraVeiculos.Test/FuncionarioModule/ControladorFuncionarioTeste.cs
@@ -90,10 +90,10 @@ namespace LocadoraVeiculos.Test.FuncionarioModule
             var gv1 = new Colaborador("lito", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
             controlador.InserirNovo(gv1);
 
-            var gv2 = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+            var gv2 = new Colaborador("zezo vitória", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "12311", "01011", "zezin", "123", new DateTime(2021, 08, 13), 4500);
             controlador.InserirNovo(gv2);
 
-            var gv3 = new Colaborador("Charles Visit", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "1231", "0101", "zezin", "123", new DateTime(2021, 08, 13), 4500);
+            var gv3 = new Colaborador("Charles Visit", "rua dos gatinhos", "zezoVitoria@gatinhos.com", "lages", "sc", "322220309", "999121315", "12312", "01012", "zezin", "123", new DateTime(2021, 08, 13), 4500);
             controlador.InserirNovo(gv3);
 
             //action

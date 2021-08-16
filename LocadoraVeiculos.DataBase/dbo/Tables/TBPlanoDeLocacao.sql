87d145392b495224fc88c193169c7d1348a57ecd
Marcos Matos <marcoscaiobel@gmail.com>
2021-08-16 15:07:45 -0300

    criada tabela plano de locacao

---
+++ LocadoraVeiculos.DataBase/dbo/Tables/TBPlanoDeLocacao.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TBPlanoDeLocacao]
+(
+	[Id]    INT          IDENTITY (1, 1) NOT NULL,
+    [Titulo]  VARCHAR (100) NOT NULL,
+    [Valor] DECIMAL (18) NOT NULL,
+    [Descricao] VARCHAR (1000) ,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)

0663c0ea0fd00cf28430ec671501697adf04312d
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-08-16 18:57:46 -0300

    criação banco de testes

---
+++ LocadoraVeiculos.TestBancoDeDados/dbo/Tables/TBCondutor.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[TBCondutor] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [NomeCondutor]  VARCHAR (100) NOT NULL,
+    [Endereco]      VARCHAR (100) NOT NULL,
+    [Email]         VARCHAR (50)  NOT NULL,
+    [Cidade]        VARCHAR (50)  NOT NULL,
+    [Estado]        VARCHAR (50)  NOT NULL,
+    [Telefone]      VARCHAR (11)  NOT NULL,
+    [Celular]       VARCHAR (12)  NOT NULL,
+    [Rg]            VARCHAR (14)  NOT NULL,
+    [Cpf]           VARCHAR (11)  NOT NULL,
+    [Cnh]           VARCHAR (20)  NOT NULL,
+    [ValidadeCnh]   DATETIME      NOT NULL,
+    [IdClienteCnpj] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TBCondutor_TBClientePJ] FOREIGN KEY ([IdClienteCnpj]) REFERENCES [dbo].[TBClientePJ] ([Id])
+);
+

3b8002659ac425c881929e732f648468bc76962b
Marcos Matos <marcoscaiobel@gmail.com>
2021-08-25 22:20:32 -0300

    implementado teste retorna todas devolucoes

--- LocadoraVeiculos.Test/DevolucaoModule/ControladorDevolucaoTeste.cs
+++ LocadoraVeiculos.Test/DevolucaoModule/ControladorDevolucaoTeste.cs
@@ -15,6 +15,7 @@ using LocadoraVeiculos.Controlador.ClienteModule.ClientePfControlador;
 using LocadoraVeiculos.Controlador.VeiculoModule;
 using LocadoraVeiculos.Controlador.ClienteModule.CondutorControlador;
 using LocadoraVeiculos.Controlador.GasolinaModule;
+using System.Collections.Generic;
 
 namespace LocadoraVeiculos.Test.DevolucaoModule
 {
@@ -82,74 +83,39 @@ namespace LocadoraVeiculos.Test.DevolucaoModule
             var devolucaoEncontrada = controlador.SelecionarPorId(devolucao.Id);
             devolucaoEncontrada.Should().Be(devolucao);
         }
+        [TestMethod]
+        public void DeveRetornar_QantidadeDeDevolucoes()
+        {
+            //arrange
+            var dataValidadeCnh = System.DateTime.Now;
+            dataValidadeCnh = dataValidadeCnh.AddYears(2);
+            var cliente = new ClientePF("ClietneTeste", "rua santa terezinha", "test@test.com", "Lages", "SC", "22222222", "22222222", "123456", "12345678966", "123456",
+                dataValidadeCnh);
+            controladorClientePF.InserirNovo(cliente);
 
-        //[TestMethod]
-        //public void DeveAtualizar_GrupoDeVeiculos()
-        //{
-        //    arrange
-        //    var grupoDeVeiculos = new GrupoDeVeiculos("SUV", 50, 20, 30, 40, 50);
-        //    controlador.InserirNovo(grupoDeVeiculos);
+            var grupoVeiculo = new GrupoDeVeiculos("vans", 60, 2, 3, 200, 4, 3);
+            byte[] imagem = { 1, 2, 3 };
+            controladorGrupoVeiculo.InserirNovo(grupoVeiculo);
 
-        //    var novoGrupoDeVeiculos = new GrupoDeVeiculos("Esportivo", 50, 20, 30, 40, 50);
+            var veiculo = new Veiculo("classic", "branco", "chevrolet", "ERA1234", "4568979", 12020, 4, 45, 5, 2012, grupoVeiculo, PortaMalaVeiculoEnum.Medio, imagem);
+            controladorVeiculo.InserirNovo(veiculo);
 
-        //    action
-        //    controlador.Editar(grupoDeVeiculos.Id, novoGrupoDeVeiculos);
+            var dataSaida = System.DateTime.Now;
+            var dataRetorno = dataSaida.AddDays(5);
+            var locacao = new Locacao(cliente, veiculo, "planoTest", dataSaida, dataRetorno, 350, 2000, 12345);
+            controladorLocacao.InserirNovo(locacao);
 
-        //    assert
-        //    GrupoDeVeiculos grupoDeVeiculosAtualizado = controlador.SelecionarPorId(grupoDeVeiculos.Id);
-        //    grupoDeVeiculosAtualizado.Should().Be(novoGrupoDeVeiculos);
-        //}
+            var tipoCombustivel = new Combustivel("gasolina", 6);
+            controladorCombustivel.InserirNovo(tipoCombustivel);
 
-        //[TestMethod]
-        //public void DeveExcluir_GrupoDeVeiculos()
-        //{
-        //    arrange
-        //    var grupoDeVeiculos = new GrupoDeVeiculos("SUV", 50, 20, 30, 40, 50);
-        //    controlador.InserirNovo(grupoDeVeiculos);
+            var devolucao = new Devolucao(locacao, tipoCombustivel, dataRetorno, 117, 254, 54);
 
-        //    action
-        //    controlador.Excluir(grupoDeVeiculos.Id);
+            //action
+            controlador.InserirNovo(devolucao);
 
-        //    assert
-        //    GrupoDeVeiculos grupoDeVeiculosEncontrado = controlador.SelecionarPorId(grupoDeVeiculos.Id);
-        //    grupoDeVeiculosEncontrado.Should().BeNull();
-        //}
-
-        //[TestMethod]
-        //public void DeveSelecionar_GrupoDeVeiculos_PorId()
-        //{
-        //    arrange
-        //    var grupoDeVeiculos = new GrupoDeVeiculos("SUV", 50, 20, 30, 40, 50);
-        //    controlador.InserirNovo(grupoDeVeiculos);
-
-        //    action
-        //    GrupoDeVeiculos grupoDeVeiculosEncontrado = controlador.SelecionarPorId(grupoDeVeiculos.Id);
-
-        //    assert
-        //    grupoDeVeiculosEncontrado.Should().NotBeNull();
-        //}
-
-        //[TestMethod]
-        //public void DeveSelecionar_TodosGrupoDeVeiculoss()
-        //{
-        //    arrange
-        //    var gv1 = new GrupoDeVeiculos("SUV", 50, 20, 30, 40, 50);
-        //    controlador.InserirNovo(gv1);
-
-        //    var gv2 = new GrupoDeVeiculos("Esportivo", 50, 20, 30, 40, 50);
-        //    controlador.InserirNovo(gv2);
-
-        //    var gv3 = new GrupoDeVeiculos("Utilitário", 50, 20, 30, 40, 50);
-        //    controlador.InserirNovo(gv3);
-
-        //    action
-        //    var grupoDeVeiculoss = controlador.SelecionarTodos();
-
-        //    assert
-        //    grupoDeVeiculoss.Should().HaveCount(3);
-        //    grupoDeVeiculoss[0].Nome.Should().Be("SUV");
-        //    grupoDeVeiculoss[1].Nome.Should().Be("Esportivo");
-        //    grupoDeVeiculoss[2].Nome.Should().Be("Utilitário");
-        //}
+            //assert
+            List<Devolucao> devolucoes = controlador.SelecionarTodos();
+            devolucoes.Should().HaveCount(1);
+        }
     }
 }

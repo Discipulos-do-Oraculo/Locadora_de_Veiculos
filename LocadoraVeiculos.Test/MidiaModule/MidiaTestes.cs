7349a19f6a654e9d88f4f227b13f7330ed3a25a4
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-08-26 16:10:55 -0300

    testes de domínio mídia e parceiros

---
+++ LocadoraVeiculos.Test/MidiaModule/MidiaTestes.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LocadoraVeiculo.Dominio.CupomModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraVeiculos.Test.MidiaModule
+{
+    [TestClass]
+    [TestCategory("Domínio")]
+
+    public class MidiaTestes
+    {
+        [TestMethod]
+        public void DeveRetornar_MidiaValida()
+        {
+            //arrange
+            Midia midia = new Midia("Internet");
+
+            //action
+            var resultado = midia.Validar();
+
+            //assert
+            resultado.Should().Be("ESTA_VALIDO");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Nome()
+        {
+            //arrange
+            Midia midia = new Midia("");
+
+            //action
+            var resultado = midia.Validar();
+
+            //assert
+            resultado.Should().Be("O campo nome é obrigatório");
+        }
+    }
+}
---
+++ LocadoraVeiculos.Test/ParceiroModule/ParceiroTestes.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using LocadoraVeiculo.Dominio.CupomModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraVeiculos.Test.ParceiroModule
+{
+    [TestClass]
+    [TestCategory("Domínio")]
+
+    public class ParceiroTestes
+    {
+        [TestMethod]
+        public void DeveRetornar_ParceiroValido()
+        {
+            //arrange
+            Midia midia = new Midia("Internet");
+            var parceiro = new Parceiro("Desconto do Deko", midia);
+
+            //action
+            var resultado = parceiro.Validar();
+
+            //assert
+            resultado.Should().Be("ESTA_VALIDO");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Nome()
+        {
+            //arrange
+            Midia midia = new Midia("Internet");
+            var parceiro = new Parceiro("", midia);
+
+            //action
+            var resultado = parceiro.Validar();
+
+            //assert
+            resultado.Should().Be("O campo nome é obrigatório");
+        }
+
+        [TestMethod]
+        public void DeveValidar_Midia()
+        {
+            //arrange
+            var parceiro = new Parceiro("Desconto do Deko", null);
+
+            //action
+            var resultado = parceiro.Validar();
+
+            //assert
+            resultado.Should().Be("O campo Mídia é obrigatório");
+        }
+    }
+}

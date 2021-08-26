4c381b2f2353e76762265133b6599648e9cd685a
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-08-26 16:11:16 -0300

    criação de telas mídia e parceiros

---
+++ LocadoraVeiculos.WindowsForms/Features/MidiaModule/TelaMidiaForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraVeiculos.WindowsForms.Features.MidiaModule
+{
+    public partial class TelaMidiaForm : Form
+    {
+        public TelaMidiaForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
